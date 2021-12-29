e59ba1af7b9575021d5e4877671514dd027d23fa
Ajdour Hassan <ajdourelhassa2096@gmail.com>
2021-12-29 01:05:30 +0100

    add new files

--- pages/contact.js
+++ pages/contact.js
@@ -4,10 +4,18 @@ import Circle from "../companents/Layout/circle";
 export default function contact() {
   return (
     <div className={classes.container}>
-      <Circle backgroundColor="red" />
       <Circle backgroundColor="crimson" />
-      <h1> Do you Have any Question? </h1>
-      <h2> Please Don't Hesitate to Contact us </h2>
+      <Circle backgroundColor="crimson" />
+      <h1 className={classes.title}> Do you Have any Question? </h1>
+      <h2 className={classes.title}> Please Don't Hesitate to Contact us </h2>
+      <form className={classes.form}>
+        <input className={classes.inputSmall} placeholder="Username" />
+        <input className={classes.inputSmall} placeholder="Phone" />
+        <input className={classes.inputLarge} placeholder="Email" />
+        <input className={classes.inputLarge} placeholder="Subject" />
+        <textarea className={classes.textarea} placeholder="Message" row={6} />
+        <button className={classes.button}>Submit</button>
+      </form>
     </div>
   );
 }
